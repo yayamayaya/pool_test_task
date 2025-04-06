acc7a6db38ed5dec1c9ea94ee2e6868aa3a4eee7
yayamayaya <semenov.aleksandr@phystech.edu>
2025-04-06 18:20:32 +0300

    сделал рефакторинг кода, добавил исключение, сделал функцию трансформации дерева в поддерево

--- src/pool_class/forest_funcs.cpp
+++ src/pool_class/forest_funcs.cpp
@@ -1,10 +1,18 @@
 #include "pool.hpp"
+#include <cassert>
 
 using namespace pooling;
 
 pool_forest::pool_tree &pool_forest::return_tree_info(pool *root)
 {
-    return forest_map[root];
+    if (root->parent)
+        return return_tree_info(root->parent);
+
+    auto it = forest_map.find(root);
+    if (it != forest_map.end())
+        return forest_map[root];
+
+    throw "tree is not in the forest";
 }
 
 void pool_forest::add_new_tree(pool *new_tree)
@@ -12,27 +20,38 @@ void pool_forest::add_new_tree(pool *new_tree)
     forest_map[new_tree] = pool_tree();
 }
 
+void pool_forest::rm_tree(pool *rmv_tree)
+{
+    forest_map.erase(rmv_tree);
+}
+
 pool *pool_forest::convert_tree_to_a_subtree(pool *tree_node)
 {
+    assert(tree_node);
+
     pool *node_parent = nullptr;
-    pool *prev_node = nullptr;
+    pool *prev_node   = nullptr;
+    pool *curr_node   = tree_node;
 
     while(1)
     {
-        node_parent = tree_node->parent;
-        tree_node->parent = prev_node;
+        node_parent = curr_node->parent;
+        curr_node->parent = prev_node;
 
-        //rm prev_node from vector
         if (prev_node)
-            tree_node->delete_child(prev_node);
-
-        if (node_parent == nullptr)
+            curr_node->delete_child(prev_node);
+        
+        if (!node_parent)
             break;
 
-        tree_node->children.push_back(node_parent);
+        curr_node->children.push_back(node_parent);
         
-        prev_node = tree_node;
-        tree_node = node_parent;
+        prev_node = curr_node;
+        curr_node = node_parent;
     }
+
+    rm_tree(curr_node);
+
+    return tree_node;
 }
 
