94adfe6295343b0ea4cc1a6fa610ed979aacc4aa
yayamayaya <semenov.aleksandr@phystech.edu>
2025-04-07 17:51:17 +0300

    изменил количество отсоединяемых бассейнов

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main()
     const size_t N = 34'000'000;
     const size_t K = 12'000'000;
     const size_t L = 27'000'000;
-    const size_t M = 25'000'000;
+    const size_t M = 3'000'000;
 
     big_data_testing unit;
 
