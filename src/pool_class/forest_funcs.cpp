8b5d92a636cd351d83f27c7c4f2358ff9b18b495
yayamayaya <semenov.aleksandr@phystech.edu>
2025-04-07 17:52:30 +0300

    вынес класс forest из класса pool

--- src/pool_class/forest_funcs.cpp
+++ src/pool_class/forest_funcs.cpp
@@ -1,9 +1,9 @@
-#include "pool.hpp"
 #include <cassert>
+#include "pool.hpp"
 
 using namespace pooling;
 
-pool::forest::tree_data &pool::forest::return_tree_info(pool *root)
+forest::tree_data &forest::return_tree_info(pool *root)
 {
     if (root->parent)
         return return_tree_info(root->parent);
@@ -15,17 +15,17 @@ pool::forest::tree_data &pool::forest::return_tree_info(pool *root)
     throw "tree is not in the forest";
 }
 
-void pool::forest::add_new_tree(pool *new_tree)
+void forest::add_new_tree(pool *new_tree)
 {
     forest_map[new_tree] = tree_data();
 }
 
-void pool::forest::rm_tree(pool *rmv_tree)
+void forest::rm_tree(pool *rmv_tree)
 {
     forest_map.erase(rmv_tree);
 }
 
-pool *pool::forest::convert_tree_to_a_subtree(pool *tree_node)
+pool *forest::convert_tree_to_a_subtree(pool *tree_node)
 {
     assert(tree_node);
 
@@ -55,12 +55,12 @@ pool *pool::forest::convert_tree_to_a_subtree(pool *tree_node)
     return tree_node;
 }
 
-void pool::forest::add_tree_link(pool *f_tree, pool *s_tree)
+void forest::add_tree_link(pool *f_tree, pool *s_tree)
 {
     linked_trees.push_back(std::pair<pool *, pool *> (f_tree, s_tree));
 }
 
-counter_t pool::forest::find_tree_link(pool *tree)
+counter_t forest::find_tree_link(pool *tree)
 {
     for (counter_t i = 0; i < linked_trees.size(); i++)
         if (linked_trees[i].first == tree || linked_trees[i].second == tree)
@@ -69,7 +69,7 @@ counter_t pool::forest::find_tree_link(pool *tree)
     return LINK_NOT_FOUND;
 }
 
-counter_t pool::forest::find_tree_link(pool *f, pool *s)
+counter_t forest::find_tree_link(pool *f, pool *s)
 {
     if (!f->side_link || !s->side_link)
         return LINK_NOT_FOUND;
