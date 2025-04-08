e59e12a6b4dea92f0dd58ad04b97db98e3fb206f
yayamayaya <semenov.aleksandr@phystech.edu>
2025-04-08 16:26:02 +0300

    обработка исключение находится в main

--- main.cpp
+++ main.cpp
@@ -12,27 +12,35 @@ int main()
     const size_t L = 27'000'000;
     const size_t M = 3'000'000;
 
-    big_data_testing unit;
-
-    unit.create_pools(N);
-
-    _LOG << "BREAKPOINT" << END_;
-
-    unit.connect_pools(K);
-    unit.dump_pools();
-
-    _LOG << "BREAKPOINT" << END_;
-
-    unit.fill_pools(L);
-    unit.dump_pools();
-
-    _LOG << "BREAKPOINT" << END_;
-
-    unit.break_connections(M);
-    unit.fill_pools(L);
-    unit.dump_pools();
-
-    _LOG << "FINISHED" << END_;
+    try
+    {
+        big_data_testing unit;
+        
+        unit.create_pools(N);
+        
+        _LOG << "BREAKPOINT" << END_;
+        
+        unit.connect_pools(K);
+        unit.dump_pools();
+        
+        _LOG << "BREAKPOINT" << END_;
+        
+        unit.fill_pools(L);
+        unit.dump_pools();
+        
+        _LOG << "BREAKPOINT" << END_;
+        
+        unit.break_connections(M);
+        unit.fill_pools(L);
+        unit.dump_pools();
+        
+        _LOG << "FINISHED" << END_;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "> Exception catch " << e.what() << std::endl;
+        _LOG[ERROR] << "> Exception catch " << e.what() << END_;
+    }
 
     std::cout << "> programm finished in";
 
